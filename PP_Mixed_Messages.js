6077c13f0fbc531a00a1e4c4bd48e5154deb6b99
Fratti <JacobRFratti@gmail.com>
2023-07-18 20:22:22 -0400

    Final

--- PP_Mixed_Messages.js
+++ PP_Mixed_Messages.js
@@ -17,28 +17,25 @@ let synWord = ["the", "and", "or", "of", "with", "in", "at", "some", "all", "is"
 let firstSentenceArr = [];
 firstSentenceArr.push((noun[Math.floor(Math.random()*noun.length)]), (synWord[Math.floor(Math.random()*synWord.length)]), (noun[Math.floor(Math.random()*noun.length)]));
 
-function firstSentence() {
-    return firstSentenceArr.join(" ");
-};
-
-console.log(firstSentence());
-
 //Second Sentence
 let secondSentenceArr = [];
 secondSentenceArr.push((verb[Math.floor(Math.random()*verb.length)]), (adverb[Math.floor(Math.random()*adverb.length)]), (adjective[Math.floor(Math.random()*adjective.length)]));
-
-function secondSentence() {
-    return secondSentenceArr.join(" ");
-};
-
-   console.log(secondSentence());
 
 // Third Sentence
 let thirdSentenceArr = [];
 thirdSentenceArr.push((synWord[Math.floor(Math.random()*synWord.length)]), (verb[Math.floor(Math.random()*verb.length)]), (synWord[Math.floor(Math.random()*synWord.length)]), (noun[Math.floor(Math.random()*noun.length)]));
 
-function thirdSentence() {
- return thirdSentenceArr.join(" ");
-};
+//Poem Function
+function randomPoem() {
+    let line1 = firstSentenceArr.join(" ");
+    let line2 = secondSentenceArr.join(" ");
+    let line3 = thirdSentenceArr.join(" ");
+    let poem = line1 + ", " + line2 + ", " + line3;
+    return poem;
+}
 
-console.log(thirdSentence());
+console.log(randomPoem());
+
+//const [line1, line2, line3] = randomPoem (firstSentenceArr.join(" "), secondSentenceArr.join(" "), thirdSentenceArr.join(" "));
+//console.log(randomPoem(line1, line2, line3));
+
